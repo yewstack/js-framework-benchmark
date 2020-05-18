88ec1cce1cb69279bc13b748233e3cdef9189962
Justin Starry <justin@solana.com>
2020-05-18 11:53:18 +0800

    Update yew project to v0.16.2

--- frameworks/non-keyed/yew/src/lib.rs
+++ frameworks/non-keyed/yew/src/lib.rs
@@ -1,19 +1,12 @@
-#![recursion_limit = "256"]
+#![recursion_limit = "1024"]
 
-extern crate byteorder;
-extern crate stdweb;
-#[macro_use]
-extern crate yew;
-extern crate rand;
-
-use byteorder::{ByteOrder, LittleEndian};
-use rand::prng::XorShiftRng;
-use rand::{Rng, SeedableRng};
+use rand::prelude::*;
 use std::cmp::min;
-use stdweb::web::Date;
 use yew::prelude::*;
+use yew::web_sys::window;
+use wasm_bindgen::prelude::wasm_bindgen;
 
-static ADJECTIVES: &[&'static str] = &[
+static ADJECTIVES: &[&str] = &[
     "pretty",
     "large",
     "big",
@@ -41,12 +34,12 @@ static ADJECTIVES: &[&'static str] = &[
     "fancy",
 ];
 
-static COLOURS: &[&'static str] = &[
+static COLOURS: &[&str] = &[
     "red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black",
     "orange",
 ];
 
-static NOUNS: &[&'static str] = &[
+static NOUNS: &[&str] = &[
     "table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger",
     "pizza", "mouse", "keyboard",
 ];
@@ -57,16 +50,13 @@ struct Row {
 }
 
 impl Row {
-    fn new<R>(id: usize, rng: &mut R) -> Row
-    where
-        R: Rng,
-    {
+    fn new(id: usize, rng: &mut SmallRng) -> Row {
         let mut label = String::new();
-        label.push_str(rng.choose(ADJECTIVES).unwrap());
+        label.push_str(ADJECTIVES.choose(rng).unwrap());
         label.push(' ');
-        label.push_str(rng.choose(COLOURS).unwrap());
+        label.push_str(COLOURS.choose(rng).unwrap());
         label.push(' ');
-        label.push_str(rng.choose(NOUNS).unwrap());
+        label.push_str(NOUNS.choose(rng).unwrap());
 
         Row { id, label }
     }
@@ -76,7 +66,8 @@ pub struct Model {
     rows: Vec<Row>,
     next_id: usize,
     selected_id: Option<usize>,
-    rng: XorShiftRng,
+    rng: SmallRng,
+    link: ComponentLink<Self>,
 }
 
 pub enum Msg {
@@ -93,46 +84,42 @@ impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        let mut seed = [0; 16];
-        LittleEndian::write_u128(&mut seed, Date::now() as u128);
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Model {
             rows: Vec::new(),
             next_id: 1,
             selected_id: None,
-            rng: XorShiftRng::from_seed([0; 16]),
+            rng: SmallRng::from_entropy(),
+            link,
         }
+    }
+
+    fn change(&mut self, _: ()) -> ShouldRender {
+        false
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::Run(amount) => {
-                let next_id = self.next_id;
                 let rng = &mut self.rng;
+                let next_id = self.next_id;
                 let update_amount = min(amount, self.rows.len());
                 for index in 0..update_amount {
                     self.rows[index] = Row::new(next_id + index, rng);
                 }
-                self.next_id += update_amount;
-                self.rows.extend(
-                    (update_amount..amount)
-                        .map(|index| Row::new(next_id + index, rng))
-                        .collect::<Vec<_>>(),
-                );
-                self.next_id += amount - update_amount;
+                self.rows
+                    .extend((update_amount..amount).map(|index| Row::new(next_id + index, rng)));
+                self.next_id += amount;
             }
             Msg::Add(amount) => {
-                let next_id = self.next_id;
                 let rng = &mut self.rng;
-                self.rows.extend(
-                    (0..amount)
-                        .map(|index| Row::new(next_id + index, rng))
-                        .collect::<Vec<_>>(),
-                );
+                let next_id = self.next_id;
+                self.rows
+                    .extend((0..amount).map(|index| Row::new(next_id + index, rng)));
                 self.next_id += amount;
             }
             Msg::Update(step) => {
-                for index in (0..(self.rows.len() / step)).map(|x| x * step) {
+                for index in (0..self.rows.len()).step_by(step) {
                     self.rows[index].label += " !!!";
                 }
             }
@@ -145,79 +132,79 @@ impl Component for Model {
                 }
             }
             Msg::Remove(id) => {
-                if let Some((index, _)) = self.rows.iter().enumerate().find(|(_, row)| row.id == id)
-                {
+                if let Some(index) = self.rows.iter().position(|row| row.id == id) {
                     self.rows.remove(index);
                 }
             }
             Msg::Select(id) => {
-                if self.selected_id == Some(id) {
-                    self.selected_id = None;
-                } else {
-                    self.selected_id = Some(id);
-                }
+                self.selected_id = Some(id);
             }
         }
         true
     }
-}
 
-impl Renderable<Model> for Model {
-    fn view(&self) -> Html<Self> {
+    fn view(&self) -> Html {
         html! {
-            <div class="container",>
-                <div class="jumbotron",>
-                    <div class="row",>
-                        <div class="col-md-6",>
+            <div class="container">
+                <div class="jumbotron">
+                    <div class="row">
+                        <div class="col-md-6">
                             <h1>{ "Yew" }</h1>
                         </div>
-                        <div class="col-md-6",>
-                            <div class="row",>
-                                <div class="col-sm-6 smallpad",>
-                                    <button type="button", class="btn btn-primary btn-block", onclick=|_| Msg::Run(1_000), id="run",>{ "Create 1,000 rows" }</button>
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col-sm-6 smallpad">
+                                    <button type="button" id="run" class="btn btn-primary btn-block" onclick=self.link.callback(|_| Msg::Run(1_000))>{ "Create 1,000 rows" }</button>
                                 </div>
-                                <div class="col-sm-6 smallpad",>
-                                    <button type="button", class="btn btn-primary btn-block", onclick=|_| Msg::Run(10_000), id="runlots",>{ "Create 10,000 rows" }</button>
+                                <div class="col-sm-6 smallpad">
+                                    <button type="button" class="btn btn-primary btn-block" onclick=self.link.callback(|_| Msg::Run(10_000)) id="runlots">{ "Create 10,000 rows" }</button>
                                 </div>
-                                <div class="col-sm-6 smallpad",>
-                                    <button type="button", class="btn btn-primary btn-block", onclick=|_| Msg::Add(1_000), id="add",>{ "Append 1,000 rows" }</button>
+                                <div class="col-sm-6 smallpad">
+                                    <button type="button" class="btn btn-primary btn-block" onclick=self.link.callback(|_| Msg::Add(1_000)) id="add">{ "Append 1,000 rows" }</button>
                                 </div>
-                                <div class="col-sm-6 smallpad",>
-                                    <button type="button", class="btn btn-primary btn-block", onclick=|_| Msg::Update(10), id="update",>{ "Update every 10th row" }</button>
+                                <div class="col-sm-6 smallpad">
+                                    <button type="button" class="btn btn-primary btn-block" onclick=self.link.callback(|_| Msg::Update(10)) id="update">{ "Update every 10th row" }</button>
                                 </div>
-                                <div class="col-sm-6 smallpad",>
-                                    <button type="button", class="btn btn-primary btn-block", onclick=|_| Msg::Clear, id="clear",>{ "Clear" }</button>
+                                <div class="col-sm-6 smallpad">
+                                    <button type="button" class="btn btn-primary btn-block" onclick=self.link.callback(|_| Msg::Clear) id="clear">{ "Clear" }</button>
                                 </div>
-                                <div class="col-sm-6 smallpad",>
-                                    <button type="button", class="btn btn-primary btn-block", onclick=|_| Msg::Swap, id="swaprows",>{ "Swap Rows" }</button>
+                                <div class="col-sm-6 smallpad">
+                                    <button type="button" class="btn btn-primary btn-block" onclick=self.link.callback(|_| Msg::Swap) id="swaprows">{ "Swap Rows" }</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <table class="table table-hover table-striped test-data",>
-                    <tbody id="tbody",>
+                <table class="table table-hover table-striped test-data">
+                    <tbody id="tbody">
                         { for self.rows.iter().map(|row| {
                             let id = row.id.clone();
                             html! {
-                                <tr class=if self.selected_id == Some(id) { "danger" } else  { "" },>
-                                    <td class="col-md-1",>{ id.to_string() }</td>
-                                    <td class="col-md-4", onclick=|_| Msg::Select(id),>
-                                        <a class="lbl",>{ row.label.clone() }</a>
+                                <tr class=if self.selected_id == Some(id) { "danger" } else  { "" }>
+                                    <td class="col-md-1">{ id.to_string() }</td>
+                                    <td class="col-md-4" onclick=self.link.callback(move |_| Msg::Select(id))>
+                                        <a class="lbl">{ row.label.clone() }</a>
                                     </td>
-                                    <td class="col-md-1",>
-                                        <a class="remove", onclick=|_| Msg::Remove(id),>
-                                            <span class="glyphicon glyphicon-remove remove", aria-hidden="true",></span>
+                                    <td class="col-md-1">
+                                        <a class="remove" onclick=self.link.callback(move |_| Msg::Remove(id))>
+                                            <span class="glyphicon glyphicon-remove remove" aria-hidden="true"></span>
                                         </a>
                                     </td>
-                                    <td class="col-md-6",></td>
+                                    <td class="col-md-6"></td>
                                 </tr>
                             }
                         } ) }
                     </tbody>
                 </table>
-                <span class="preloadicon glyphicon glyphicon-remove", aria-hidden="true",></span>
+                <span class="preloadicon glyphicon glyphicon-remove" aria-hidden="true"></span>
             </div>
         }
     }
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    let document = window().unwrap().document().unwrap();
+    let mount_el = document.query_selector("#main").unwrap().unwrap();
+    App::<Model>::new().mount(mount_el);
 }
